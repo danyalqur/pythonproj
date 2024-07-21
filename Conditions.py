9cc5a8cb997f819f5c3385a4585a9d3c57b38ac3
classymandan <50890175+classymandan@users.noreply.github.com>
2024-07-21 03:59:36 +0100

    Created Conditions.py file to go over conditions

---
+++ Conditions.py
@@ -0,0 +1,8 @@
+# Conditions
+
+# Python uses boolean logic to evaluate conditions. The boolean values True and False are returned when an expression is compared or evaluated. For example:
+# x = 2
+# print(x == 2) # prints out True
+# print(x == 3) # prints out False
+# print(x < 3) # prints out True
+
