5dc5853170a20391a1d3a46684c33d009afab0f0
Danyal <classymandan@gmail.com>
2024-07-28 15:42:13 +0100

    Completed Functions.py . Added and completed Classes_and_Objects.py

--- Functions.py
+++ Functions.py
@@ -30,4 +30,48 @@
 
 # my_function_with_args("Danyal", "A happy sunday morning!")
 
+# How do you call functions in Python?
+# Simply write the function's name followed by (), placing any required arguments within the brackets. For example, lets call the functions from the previous
+# sections written above
 
+# # Define our 3 functions
+# def my_function():
+#     print("Hello From My Function!")
+
+# def my_function_with_args(username, greeting):
+#     print("Hello, %s, From My Function!, I wish you %s"%(username, greeting))
+
+# def sum_two_numbers(a, b):
+#     return a + b
+
+# # print(a simple greeting)
+# my_function()
+
+# #prints - "Hello, John Doe, From My Function!, I wish you a great year!"
+# my_function_with_args("John Doe", "a great year!")
+
+# # after this line x will hold the value 3!
+# x = sum_two_numbers(1,2)
+
+# Exercise
+# In this exercise you'll use an existing function, and then add your own to create a fully functional program.
+# 1. Add a function named list_benefits() that returns the following list of strings: "More organized code", "More readable code", "Easier code reuse", 
+#  "Allowing programmers to share and connect code together"
+# 2. Add a function named build_sentence(info) which revieves a single argument containing a string and returns a sentence starting with the given string
+#  " is a benefit of functions"
+# 3. Run and see all the functions work together!
+
+# Modify this function to return a list of strings as defined above
+def list_benefits():
+    return["More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"]
+
+# Modify this function to concatenate to each benefit - " is a benefit of functions!"
+def build_sentence(benefit):
+    return(benefit + " is a benefit of functions!")
+
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    for benefit in list_of_benefits:
+        print(build_sentence(benefit))
+
+name_the_benefits_of_functions()
